1107874d897691e28826835f2a722aef87f241ca
vikramriyer <vikram.iyer09@gmail.com>
2017-07-30 17:05:14 +0530

    feat: train test split, feature scaling added in this part of the commit

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -53,20 +53,19 @@ from sklearn.preprocessing import OneHotEncoder
 onehotencode = OneHotEncoder()
 X = onehotencode.fit_transform(X).toarray()
 
-print X
-
 # lets do the encoding for purchases column
 labelencode_y = LabelEncoder()
 y = labelencode_y.fit_transform(y)
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+X_train = sc_X.fit_transform(X_train.reshape(-1,1))
+X_test = sc_X.transform(X_test.reshape(-1,1))
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))
+y_test = sc_y.fit_transform(y_test.reshape(-1,1))
